5a20ab9bcc0cb3d771a3941c76555bdaed9c103f
Alex Rudoi <sanikbrant@gmail.com>
2020-01-29 10:33:38 +0200

    Setting constraints on the start screen

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -9,13 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var swooshImage: UIImageView!
+    
     var design: Design!
     var background = UIImage(named: "welcomeScreenBG.png") 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        settingBackground()
+        
+        swooshImage.frame = CGRect(x: view.frame.size.width / 2 - swooshImage.frame.size.width, y: 50, width: swooshImage.frame.size.width, height: swooshImage.frame.size.height)
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    func settingBackground() {
         var imageView : UIImageView!
         imageView = UIImageView(frame: view.bounds)
         imageView.contentMode =  UIView.ContentMode.scaleAspectFill
@@ -24,12 +36,6 @@ class ViewController: UIViewController {
         imageView.center = view.center
         view.addSubview(imageView)
         self.view.sendSubviewToBack(imageView)
-        
     }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-
 }
 
