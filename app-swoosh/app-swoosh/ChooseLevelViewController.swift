4a642218aa519eee4f3575aba61f7a4149290bb4
Alex Rudoi <sanikbrant@gmail.com>
2020-01-29 22:06:36 +0200

    Added some screens

---
+++ app-swoosh/app-swoosh/ChooseLevelViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ChooseLevelViewController.swift
+//  app-swoosh
+//
+//  Created by Alex Rudoi on 291//20.
+//  Copyright © 2020 Alex Rudoi. All rights reserved.
+//
+
+import UIKit
+
+class ChooseLevelViewController: UIViewController {
+
+    var background = UIImage(named: "beginnerBallerBG.png")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        settingBackground(backround: background!)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func settingBackground(backround: UIImage) {
+        self.background = backround
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
+
+}
